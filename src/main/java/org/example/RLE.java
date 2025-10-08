5b7d35ca6c96543f29fe401e58a7eb31b35dfa85
samsonchan <samsonc2027@student.cis.edu.hk>
2025-10-08 09:55:18 +0800

    Done

--- src/main/java/org/example/RLE.java
+++ src/main/java/org/example/RLE.java
@@ -25,13 +25,50 @@ public class RLE {
         return sb.toString();
     }
 
-    /** TODO 1: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to compress a String. Returns the compressed String. */
-    public static String compress(String uncompressed) { return null; }
+        /** TODO 1: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to compress a String. Returns the compressed String. */
+        public static String compress(String uncompressed) {
+            if (uncompressed == null || uncompressed.isEmpty()) {
+                return "";
+            }
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            while (i < uncompressed.length()) {
+                char c = uncompressed.charAt(i);
+                int count = 0;
+                while (i < uncompressed.length() && uncompressed.charAt(i) == c) {
+                    count++;
+                    i++;
+                }
+                sb.append(c);
+                sb.append(count);
+            }
+            return sb.toString();
+        }
 
-    /** TODO 2: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to decompress a String. Returns the uncompressed String. */
-    public static String decompress(String compressed) {
-        return null;
+        /** TODO 2: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to decompress a String. Returns the uncompressed String. */
+        public static String decompress(String compressed) {
+            if (compressed == null || compressed.isEmpty()) {
+                return "";
+            }
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            while (i < compressed.length()) {
+                if (!Character.isLetter(compressed.charAt(i))) {
+                    i++;
+                    continue;
+                }
+                char c = compressed.charAt(i);
+                i++;
+                int num = 0;
+                while (i < compressed.length() && Character.isDigit(compressed.charAt(i))) {
+                    num = num * 10 + (compressed.charAt(i) - '0');
+                    i++;
+                }
+                for (int j = 0; j < (num == 0 ? 1 : num); j++) {
+                    sb.append(c);
+                }
+            }
+            return sb.toString();
+        }
     }
 
-
-}
